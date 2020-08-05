85c37f826690d1eb4facf28e041222b292bc2dee
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-08-05 14:34:08 +0200

    update dependensies

--- packages/ui/src/types/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/*
- * @license
- * Copyright Ahmed Elywa. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-exports.__esModule = true;
